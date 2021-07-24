8e44a4eed195e0b0cf18bb9111c327078cb18b98
WHIPLASHCZ <2238907215@qq.com>
2021-07-24 16:02:26 +0800

    add file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    我日你哥
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 export default {
-  name: 'Home',
+  name: "Home",
+  created() {
+    console.log("home");
+    console.log(axios);
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
